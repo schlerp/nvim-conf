ec1f092331470648e0bbe02a7a7ee129593de867
PattyC <patrickcoffey91@gmail.com>
2023-08-28 11:34:49 +0930

    removed mypy by default

--- nvim/lua/core/langs/python.lua
+++ nvim/lua/core/langs/python.lua
@@ -3,23 +3,21 @@ local M = {
 	lang_name = "python",
 
 	formatters = {
-        "black",
-        "isort",
+		"black",
+		"isort",
 	},
 
-	linters = {
-		"mypy",
-	},
+	linters = {},
 
 	lsp_servers = {
 		{
 			lsp_name = "pyright",
 			lsp_settings = {},
 		},
-        {
-            lsp_name = "ruff_lsp",
-            lsp_settings = {},
-        },
+		{
+			lsp_name = "ruff_lsp",
+			lsp_settings = {},
+		},
 	},
 }
 
