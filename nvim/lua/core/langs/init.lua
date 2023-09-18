10b87ef587f4c44ba21a96f325a0c0a295639c21
PattyC <patrickcoffey91@gmail.com>
2023-09-18 22:22:12 +0930

    did the thing

--- nvim/lua/core/langs/init.lua
+++ nvim/lua/core/langs/init.lua
@@ -1,9 +1,29 @@
-local lang_config = require("config.langs")
+---@type NvimConfig
+local config = require("core.config") --- type: ignore
 local utils = require("core.utils")
 local M = {}
 
+-- if the language config is nil then we will use the default (enable all)
+if config.lang_config == nil then
+	config.lang_config = {
+		enabled_langs = {
+			"lua",
+			"python",
+			"rust",
+			"markdown",
+			"svelte",
+			"html",
+			"typescript",
+			"bash",
+			"cpp",
+			"dockerfile",
+		},
+		custom_lang_defs = {},
+	}
+end
+
 -- load the builtin langues that have been enabled
-for _, value in ipairs(lang_config.enabled_langs) do
+for _, value in ipairs(config.lang_config.enabled_langs) do
 	local this_lang_config = utils.try_import_module("core.langs." .. value)
 	if this_lang_config ~= nil then
 		M[this_lang_config.lang_name] = this_lang_config
