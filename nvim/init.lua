99e20a5a937d595d625057697fd908b44658cab5
PattyC <patrickcoffey91@gmail.com>
2023-09-22 14:32:34 +0930

    moved the rest of the plugins to lazy loading

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,11 +3,6 @@ require("core.set")
 
 -- setup plugins
 require("core.plugins")
-require("core.configs.init")
-
 
 -- setup keymaps
 require("core.keymap")
-
--- setup terminal
-require("core.terminal")
