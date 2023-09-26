fc141249a30124dead0a6955de1d8e6aae7a9df1
PattyC <2208036+schlerp@users.noreply.github.com>
2023-09-26 20:15:00 +0930

    refactoring theme settings into general set.lua module (#20)

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -9,7 +9,6 @@ local plugins = {
         "catppuccin/nvim",
         as = "catppuccin",
         config = function()
-            vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
             require("catppuccin").setup()
             vim.api.nvim_command("colorscheme catppuccin")
         end,
