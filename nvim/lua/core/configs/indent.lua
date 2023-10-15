e9a7259bfd439338e9c3a729a061419341e7fac4
PattyC <2208036+schlerp@users.noreply.github.com>
2023-10-15 08:10:03 +0000

    Update IBL to use v3, updated config to match (#22)
    * Update indent-blankline to v3.x
    
    * Updated ibl config to be compatible with v3

--- nvim/lua/core/configs/indent.lua
+++ nvim/lua/core/configs/indent.lua
@@ -2,14 +2,16 @@ require("ibl").setup({
     show_end_of_line = true,
     show_current_context = true,
     space_char_blankline = " ",
-    buftype_exclude = { "terminal", "nofile", "quickfix", "prompt" },
-    filetype_exclude = {
-        "lspinfo",
-        "packer",
-        "checkhealth",
-        "help",
-        "man",
-        "alpha",
-        "",
+    exclude = {
+        filetypes = {
+            "lspinfo",
+            "packer",
+            "checkhealth",
+            "help",
+            "man",
+            "alpha",
+            "",
+        },
+        buftypes = { "terminal", "nofile", "quickfix", "prompt" },
     },
 })
