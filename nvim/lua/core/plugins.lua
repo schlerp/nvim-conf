81257d8a5a668388836be4cbbcaaa03abd659891
PattyC <patrickcoffey91@gmail.com>
2023-11-06 21:33:30 +0930

    Adding support for sourcegraph plugin

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -141,6 +141,18 @@ local plugins = {
         event = "VeryLazy",
     },
 
+    -- sourcegraph plugin
+    {
+        "sourcegraph/sg.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim",
+        },
+
+        -- If you have a recent version of lazy.nvim, you don't need to add this!
+        build = "nvim -l build/init.lua",
+    },
+
     -- scrollbars
     {
         "petertriho/nvim-scrollbar",
