7ba4c07ad582d5de2c4db9fcf7407a8dadf055e2
PattyC <2208036+schlerp@users.noreply.github.com>
2023-11-07 10:34:54 +0930

    Add sourcegraph, copilot and refactor some plugins to be optional (#26)
    * add sourcegraph plugin
    * add copilot
    * refactor plugin config to enable toggling some plugins via nvim/config/features.lua

---
+++ nvim/lua/config/features.lua
@@ -0,0 +1,10 @@
+---@type NvimFeatureConfig
+local M = {
+    use_copilot = true,
+    use_scrollbars = true,
+    use_neogit = false,
+    use_source_graph = true,
+    use_start_page = true,
+}
+
+return M
--- nvim/lua/core/config.lua
+++ nvim/lua/core/config.lua
@@ -5,5 +5,6 @@ local config = {}
 
 config.lang_config = utils.try_import_module("config.langs")
 config.keymap_config = utils.try_import_module("config.keys")
+config.feature_config = utils.try_import_module("config.features")
 
 return config
