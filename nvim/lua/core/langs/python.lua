9b35f0a7d178031fc841ad662cf8fc72c381c9ae
PattyC <patrickcoffey91@gmail.com>
2024-01-22 10:01:53 +0930

    feat: swap from pyright to pylsp

--- nvim/lua/core/langs/python.lua
+++ nvim/lua/core/langs/python.lua
@@ -10,8 +10,28 @@ local M = {
 
     lsp_servers = {
         {
-            lsp_name = "pyright",
-            lsp_settings = {},
+            lsp_name = "pylsp",
+            lsp_settings = {
+                settings = {
+                    pylsp = {
+                        plugins = {
+                            flake8 = {
+                                enabled = false,
+                                config = "$HOME/projects/kraken-core/src/setup.cfg",
+                            },
+                            pyls_isort = { enabled = true },
+                            pylsp_mypy = {
+                                enabled = true,
+                                dmypy = true,
+                                live_mode = false,
+                            },
+                        },
+                    },
+                },
+                flags = {
+                    debounce_text_changes = 200,
+                },
+            },
         },
         {
             lsp_name = "ruff_lsp",
