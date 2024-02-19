780bcd2ca3b87c427770c466ee0b225966878ee1
PattyC <patrickcoffey91@gmail.com>
2024-02-19 12:55:58 +0930

    feat: move from catpuccin to gruvbox

---
+++ nvim/lua/core/configs/gruvbox.lua
@@ -0,0 +1,31 @@
+require("gruvbox").setup({
+    terminal_colors = true, -- add neovim terminal colors
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = {
+        strings = false,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+    },
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    invert_intend_guides = false,
+    inverse = true, -- invert background for search, diffs, statuslines and errors
+    contrast = "", -- can be "hard", "soft" or empty string
+    palette_overrides = {},
+    overrides = {},
+    dim_inactive = true,
+    transparent_mode = false,
+})
+
+-- these must be called after the setup function
+-- we want dark mode
+vim.o.background = "dark"
+
+-- set the colour scheme to gruvbox
+vim.api.nvim_command("colorscheme gruvbox")
