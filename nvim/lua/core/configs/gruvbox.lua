5e84f97021b848515c1a0fa4bde7923e0ebeec5e
PattyC <patrickcoffey91@gmail.com>
2024-02-20 10:38:33 +0930

    feat: remove diming for inactive gruvbox

--- nvim/lua/core/configs/gruvbox.lua
+++ nvim/lua/core/configs/gruvbox.lua
@@ -13,13 +13,13 @@ require("gruvbox").setup({
     strikethrough = true,
     invert_selection = false,
     invert_signs = false,
-    invert_tabline = false,
+    invert_tabline = true,
     invert_intend_guides = false,
     inverse = false, -- invert background for search, diffs, statuslines and errors
     contrast = "", -- can be "hard", "soft" or empty string
     palette_overrides = {},
     overrides = {},
-    dim_inactive = true,
+    dim_inactive = false,
     transparent_mode = false,
 })
 
