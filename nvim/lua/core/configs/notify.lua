877fd85a70552da87419566b24f2325f04fe222b
PattyC <2208036+schlerp@users.noreply.github.com>
2024-02-20 12:12:32 +0930

    fix: update vim notify to work better (#45)

--- nvim/lua/core/configs/notify.lua
+++ nvim/lua/core/configs/notify.lua
@@ -1,6 +1,9 @@
 vim.notify = require("notify")
 
-vim.notify.setup()
+vim.notify.setup({
+    stages = "static",
+    render = "wrapped-compact",
+})
 
 vim.lsp.handlers["window/showMessage"] = function(_, result, ctx)
     local client = vim.lsp.get_client_by_id(ctx.client_id)
