cc00e62ad599d2a55e2fd2050b1a29c9ed88c16d
PattyC <patrickcoffey91@gmail.com>
2024-04-14 10:47:05 +0930

    Remove some plugins for replacement in future PR's

--- nvim/lua/core/diagnostics.lua
+++ nvim/lua/core/diagnostics.lua
@@ -20,10 +20,3 @@ for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
-
-vim.keymap.set(
-    "n",
-    "<Leader>dl",
-    require("lsp_lines").toggle,
-    { desc = "Toggle lsp_lines" }
-)
