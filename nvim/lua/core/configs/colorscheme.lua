7595a798f80e0f19d4e0b39c5ce138433769d01a
PattyC <patrickcoffey91@gmail.com>
2024-06-07 09:35:16 +0930

    feat: update colour scheme to use kanagawa

--- nvim/lua/core/configs/colorscheme.lua
+++ nvim/lua/core/configs/colorscheme.lua
@@ -18,7 +18,7 @@ require("kanagawa").setup({
     end,
     theme = "wave", -- Load "wave" theme when 'background' option is not set
     background = { -- map the value of 'background' option to a theme
-        dark = "dragon", -- try "dragon" !
+        dark = "wave", -- try "dragon" !
         light = "lotus",
     },
 })
