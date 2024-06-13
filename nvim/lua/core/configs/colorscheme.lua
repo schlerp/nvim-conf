546cc97effc7f8528455098396a8823596b5ab36
PattyC <2208036+schlerp@users.noreply.github.com>
2024-06-13 21:17:09 +0930

    feat: change theme to nordic and add noice (#65)

--- nvim/lua/core/configs/colorscheme.lua
+++ nvim/lua/core/configs/colorscheme.lua
@@ -1,31 +1,54 @@
-require("kanagawa").setup({
-    compile = true, -- enable compiling the colorscheme
-    undercurl = true, -- enable undercurls
-    commentStyle = { italic = true },
-    functionStyle = {},
-    keywordStyle = { italic = true },
-    statementStyle = { bold = true },
-    typeStyle = {},
-    transparent = false, -- do not set background color
-    dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-    terminalColors = true, -- define vim.g.terminal_color_{0,17}
-    colors = { -- add/modify theme and palette colors
-        palette = {},
-        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+require("nordic").setup({
+    -- This callback can be used to override the colors used in the palette.
+    on_palette = function(palette) return palette end,
+    -- Enable bold keywords.
+    bold_keywords = true,
+    -- Enable italic comments.
+    italic_comments = true,
+    -- Enable general editor background transparency.
+    transparent_bg = false,
+    -- Enable brighter float border.
+    bright_border = false,
+    -- Reduce the overall amount of blue in the theme (diverges from base Nord).
+    reduced_blue = true,
+    -- Swap the dark background with the normal one.
+    swap_backgrounds = false,
+    -- Override the styling of any highlight group.
+    override = {},
+    -- Cursorline options.  Also includes visual/selection.
+    cursorline = {
+        -- Bold font in cursorline.
+        bold = true,
+        -- Bold cursorline number.
+        bold_number = true,
+        -- Available styles: 'dark', 'light'.
+        theme = "dark",
+        -- Blending the cursorline bg with the buffer bg.
+        blend = 0.85,
     },
-    overrides = function(colors) -- add/modify highlights
-        return {}
-    end,
-    theme = "wave", -- Load "wave" theme when 'background' option is not set
-    background = { -- map the value of 'background' option to a theme
-        dark = "wave", -- try "dragon" !
-        light = "lotus",
+    noice = {
+        -- Available styles: `classic`, `flat`.
+        style = "flat",
+    },
+    telescope = {
+        -- Available styles: `classic`, `flat`.
+        style = "flat",
+    },
+    leap = {
+        -- Dims the backdrop when using leap.
+        dim_backdrop = false,
+    },
+    ts_context = {
+        -- Enables dark background for treesitter-context window
+        dark_background = true,
     },
 })
+
+require("nordic").load()
 
 -- these must be called after the setup function
 -- we want dark mode
 vim.o.background = "dark"
 
 -- set the colour scheme to gruvbox
-vim.api.nvim_command("colorscheme kanagawa")
+vim.api.nvim_command("colorscheme nordic")
