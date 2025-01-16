0903157b51c938db4526848097906177717aab91
PattyC <patrickcoffey91@gmail.com>
2025-01-16 13:23:03 +0930

    fix: remove print() call from treesitter config

--- nvim/lua/core/configs/treesitter.lua
+++ nvim/lua/core/configs/treesitter.lua
@@ -1,7 +1,5 @@
 local lang_configs = require("core.langs.init")
 
-print(vim.inspect(lang_configs))
-
 local lang_names = {}
 for _, config in pairs(lang_configs) do
     local treesitter_lang = config.lang_name
