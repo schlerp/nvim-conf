783557ba73332e50664205922aa54910a04e9b54
PattyC <patrickcoffey91@gmail.com>
2025-01-16 13:37:27 +0930

    feat: improve lsp server loading code

--- nvim/lua/core/lsp/lsp_servers.lua
+++ nvim/lua/core/lsp/lsp_servers.lua
@@ -3,6 +3,15 @@ local lspconfig = require("lspconfig")
 
 for _, config in pairs(lang_configs) do
     for _, lsp_definition in pairs(config.lsp_servers) do
-        lspconfig[lsp_definition.lsp_name].setup(lsp_definition.lsp_settings)
+        local lsp = lspconfig[lsp_definition.lsp_name]
+        if lsp.setup == nil then
+            print("No setup for " .. lsp_definition.lsp_name )
+        else
+            local status, err = pcall(lsp.setup, lsp_definition.lsp_settings)
+            if not status then
+                print("Error setting up LSP: " .. lsp_definition.lsp_name)
+                print(err)
+            end
+        end
     end
 end
