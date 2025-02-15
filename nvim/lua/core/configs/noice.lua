f5ab03e846db37dfe2287cd3fbaee579b551b60c
PattyC <2208036+schlerp@users.noreply.github.com>
2025-02-15 22:27:36 +0930

    feat: update noice to show siganture help (#83)

--- nvim/lua/core/configs/noice.lua
+++ nvim/lua/core/configs/noice.lua
@@ -143,7 +143,7 @@ require("noice").setup({
             opts = {}, -- merged with defaults from documentation
         },
         signature = {
-            enabled = false,
+            enabled = true,
             auto_open = {
                 enabled = true,
                 trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
