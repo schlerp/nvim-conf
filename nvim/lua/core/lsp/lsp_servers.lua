4ccdd8324cacd6fdcab1b0936144badfc81de1ea
PattyC <patrickcoffey91@gmail.com>
2025-10-18 15:18:28 +0930

    fix: fix deprecation of lspconfig module

--- nvim/lua/core/lsp/lsp_servers.lua
+++ nvim/lua/core/lsp/lsp_servers.lua
@@ -1,12 +1,10 @@
 local lang_configs = require("core.langs.init")
-local lspconfig = require("lspconfig")
+local lspconfig = vim.lsp.config
 
 for _, config in pairs(lang_configs) do
     for _, lsp_definition in pairs(config.lsp_servers) do
         local lsp = lspconfig[lsp_definition.lsp_name]
-        if lsp.setup == nil then
-            print("No setup for " .. lsp_definition.lsp_name )
-        else
+        if lsp.setup ~= nil then
             local status, err = pcall(lsp.setup, lsp_definition.lsp_settings)
             if not status then
                 print("Error setting up LSP: " .. lsp_definition.lsp_name)
